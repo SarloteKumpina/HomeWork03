cfad6849c8a81e52cfa15a8c37793c005ae7caf6
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-05 20:12:46 +0300

    added js controls

--- javscript.js
+++ javscript.js
@@ -1,24 +1,39 @@
 console.log("Running FizzBuzz");
 
 //globals
-const maxCountEl = document.getElementById("max-value");
-let maxCount = 100;
-let minCount = 1;
-const max = 100;
+const max = 101;
 const min = 0;
 
+const countElMin = document.getElementById("min-value");
+const countElMax = document.getElementById("max-value");
+let maxCount = 100;
+let minCount = 1;
 
+const countFizzMinMax = document.getElementById("fizz-value");
+const countBuzzMinMax = document.getElementById("buzz-value");
+let fizzNum = 3;
+let buzzNum = 5;
+
+console.log(maxCount, typeof maxCount);
 function main() {
     console.log("Running function main");
 
         
 
 
-    addElements();
-    onUserClickAddElements();
+    onUserClickAdd();
+    onUserClickClear();
+    countElMin.onchange = onMinChange;
+    onMinChange();
+    countElMax.onchange = onMaxChange;
+    onMaxChange();
+    countFizzMinMax.onchange = onFizzChange;
+    onFizzChange();
+    countBuzzMinMax.onchange = onBuzzChange;
+    onBuzzChange();
 }
 
-main();
+
 
 function addElement(parent, tag, id, classList, content) {
     const newEl = document.createElement(tag);
@@ -29,18 +44,18 @@ function addElement(parent, tag, id, classList, content) {
 }
 
 function addElements() {
-    console.log("Generating Elements");
+    console.log("Adding Elements");
     const innerCont = document.getElementById("id-inner-cont");
-    for (let i = 0; i <= maxCount; i++){  
+    for (let i = minCount; i <= maxCount; i++){  
         const id = "box-id-" + i;
         const classList = ["box"];
-        if (i % 3 === 0 && i % 5 === 0 ) {
+        if (i % fizzNum === 0 && i % buzzNum === 0 ) {
             classList.push("orange-bg");
-            addElement(innerCont, "div", id, classList, "FizzBuzz" + i);
-        }else if (i % 3 === 0) {
+            addElement(innerCont, "div", id, classList, "FizzBuzz  " + i);
+        }else if (i % fizzNum === 0) {
             classList.push("yellow-bg");
             addElement(innerCont, "div", id, classList, "Fizz" + i);
-        }else if (i % 5 === 0) {
+        }else if (i % buzzNum === 0) {
             classList.push("green-bg");
             addElement(innerCont, "div", id, classList, "Buzz" + i);
         }else {
@@ -50,11 +65,64 @@ function addElements() {
     }
 }
 
-function onUserClickAddElements () {
-    console.log("Generating elements on click");
+function onUserClickAdd() {
+    console.log("Adding elements on click");
     const btnGen = document.getElementById("btn-generate");
     btnGen.onclick = addElements;
 
 }
 
+function clearElements() {
+    console.log("Clearing Elements");
+    const innerCont = document.getElementById("id-inner-cont");
+    while (innerCont.firstChild) {
+        innerCont.removeChild(innerCont.firstChild);
+    }
+}
+
+function onUserClickClear() {
+    console.log("Clearing elements on click");
+    const btnClear = document.getElementById("btn-clear");
+    btnClear.onclick = clearElements;
+}
+
+function onMinChange() {
+    console.log("New value might be", countElMin.value);
+    const elvalue = parseInt(countElMin.value);
+    if (elvalue > max || elvalue < min) return;
+    minCount = elvalue;
+    console.log("Actually maxcount is", minCount);
+    console.log(minCount, typeof minCount);
+}
+
+function onMaxChange() {
+    console.log("New value might be", countElMax.value);
+    const elvalue = parseInt(countElMax.value);
+    if (elvalue > max || elvalue < min) return;
+    maxCount = elvalue;
+    console.log("Actually maxcount is", maxCount);
+    console.log(maxCount, typeof maxCount);
+}
+
+function onFizzChange() {
+    console.log("New value might be", countFizzMinMax.value);
+    const fvalue = parseInt(countFizzMinMax.value);
+    if (fvalue > max || fvalue < min) return;
+    fizzNum = fvalue;
+    console.log("Actually fizzcount is", fizzNum);
+    console.log(fizzNum, typeof fizzNum);
+}
+
+function onBuzzChange() {
+    console.log("New value might be", countBuzzMinMax.value);
+    const bvalue = parseInt(countBuzzMinMax.value);
+    if (bvalue > max || bvalue < min) return;
+    buzzNum = bvalue;
+    console.log("Actually buzzcount is", buzzNum);
+    console.log(buzzNum, typeof buzzNum);
+}
+
+
+
 main();
+
